8af53bf601c1e680f8a57ca7f18e8b410e9f1ca2
wen2ms <1819509111@qq.com>
2024-12-30 17:54:17 +0800

    Feat: constexpr

---
+++ constexpr.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+constexpr int factorial(int n) {
+    return (n <= 1) ? 1 : n * factorial(n - 1);
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Uasge: " << argv[0] << " number" << std::endl;
+        return 1;
+    }
+
+    int input_number = std::stoi(argv[1]); 
+    int output_number = factorial(input_number);  // computed at run time
+
+    constexpr int regulation = factorial(-1);  // computed at compile time
+
+    std::cout << "The factorial of " << input_number <<  " is " << output_number << std::endl;
+
+    std::cout << "The factorial of -1 is " << regulation << std::endl;
+
+    return 0;
+}
