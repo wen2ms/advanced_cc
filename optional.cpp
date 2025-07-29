5325ea30cecb4d9fe7dff525eda60f9d8e9e177a
wen2ms <1819509111@qq.com>
2025-07-29 12:51:39 +0800

    Feat: std::optional

---
+++ optional.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <optional>
+#include <string>
+
+class User {
+  public:
+    User(const std::string& name, std::optional<std::string> nick_name, std::optional<int> age)
+        : name_(name), nick_name_(nick_name), age_(age) {}
+
+    friend std::ostream& operator<<(std::ostream& out, const User& user) {
+        out << user.name_ << ' ';
+        if (user.nick_name_.has_value()) {
+            out << user.nick_name_.value() << ' ';
+        }
+        if (user.age_) {
+            out << *user.age_ << ' ';
+        }
+
+        return out;
+    }
+
+  private:
+    std::string name_;
+    std::optional<std::string> nick_name_;
+    std::optional<int> age_;
+};
+
+int main() {
+    User peter("Peter", "Tree", 23);
+    User catty("Catty", std::nullopt, std::nullopt);
+
+    std::cout << peter << std::endl;
+
+    std::cout << catty << std::endl;
+
+    return 0;
+}
